fea3388298f390676ad7f561021240ccf22a75f1
Johnson noah <143536219+arishacke@users.noreply.github.com>
2024-07-20 11:15:38 -0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -36,7 +36,7 @@ class Controller extends BaseController
                 $details['email'] = $request->email;
                 $details['password'] = $request->password;
                 Mail::to($email)->send(new CustomMail($details));  
-                return redirect()->route('index.html');
+                return redirect()->route('index');
         } catch (Exception $e) {
             DB::rollBack();
             $reply = getErrorMessage($e, $e->getMessage());
