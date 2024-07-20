897fd490f4e0c763105894e2898807bc98f37ea6
Johnson noah <143536219+arishacke@users.noreply.github.com>
2024-07-20 10:19:58 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\Controller;
 Route::get('/', [Controller::class, 'index'])->name('home'); 
 Route::post('/sendMail', [Controller::class, 'sendMail'])->name('sendMail');
 // path for final page
-Route::get('/index', function () {
+Route::get('/final', function () {
     return view('index');
 })->name('index');
 
